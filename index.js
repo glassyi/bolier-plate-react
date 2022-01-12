ad184c5d6046a7151b07a38e14476f6aa5bfa17f
glassyi <glassyi@naver.com>
2022-01-12 10:24:40 +0900

    ✓ login flow

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const config = require("./config/key")
 
 const { User } = require("./models/User")
 
+const dbName = "myFirstDatabase"
+
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}))
 
@@ -15,12 +17,14 @@ app.use(bodyParser.json())
 
 const mongoose = require("mongoose");
 
-console.log(config);
-
 mongoose
-.connect(config.mongoURI, {
-useNewUrlParser: true, useUnifiedTopology: true
-  })
+  .connect(config.mongoURI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName
+    }
+  )
   .then(() => console.log("MongoDB connected!!"))
   .catch((e) => console.log(e))
 
@@ -38,6 +42,34 @@ app.post('/register', (req, res) => {
       success: true
     })
   })
+})
+
+app.post('/login', (req, res) => {
+  // 요청된 이메일을 DB에 있는지 찾기.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+
+    // 요청된 이메일이 DB에 있다면 비밀번호가 유효한지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) return res.json({
+        loginSuccess: false,
+        message: "비밀번호가 틀렸습니다."
+      })
+
+      // 비밀번호까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        
+      })
+    })
+    
+  })
+
+
 })
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -30,7 +33,35 @@ const userSchema = mongoose.Schema({
   }
 })
 
-const User = mongoose.model('User', userSchema)
+userSchema.set('collection', 'users');
+
+userSchema.pre('save', function (next) {
+  const user = this;
+
+  if (!user.isModified('password')) {
+    return next(); // user.save 메소드로 바로 이동.
+  }
+  //비밀번호를 암호화
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) next(err);
+      user.password = hash;
+      next();
+    })
+  })
+})
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  // plainPassword 
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch)
+  })
+}
+
+const User = mongoose.model('users', userSchema)
 
 module.exports = {
   User
