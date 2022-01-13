c065f03ea9523129ba8f6d86f726afa549d05089
glassyi <glassyi@naver.com>
2022-01-13 17:10:49 +0900

    ✓ create auth middleware, logout api

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const bodyParser = require("body-parser");
-
-const config = require("./config/key")
-
-const { User } = require("./models/User")
+const cookieParser = require("cookie-parser");
+const config = require("./config/key");
+const { User } = require("./models/User");
+const { auth } = require("./middleware/auth");
 
 const dbName = "myFirstDatabase"
 
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 // application/json
 app.use(bodyParser.json())
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 
@@ -63,13 +64,45 @@ app.post('/login', (req, res) => {
 
       // 비밀번호까지 맞다면 토큰을 생성하기.
       user.generateToken((err, user) => {
-        
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지 등등...
+        res.cookie("x_auth", user.token)
+          .status(200)
+          .json({loginSuccess: true, userId: user._id})
       })
     })
-    
   })
+})
 
 
+// role 1 어드민, role 2 특정부서 어드민
+// role 0 일반유저, 0이 아니면 관리자
+app.get('api/users/auth', auth, (req, res) => { // auth (middleware)
+  // 여기까지 미들웨어 통과해 온것은 Authentication 이 True 라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.user._id },
+    { token: "" },
+    (err, user) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      })
+    }
+  )
 })
 
 app.listen(port, () => {
